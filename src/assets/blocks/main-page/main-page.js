e9d05c238f84df5276e2e77346d044824d84c0f2
Nikita <nle@nkhstudio.ru>
2019-06-07 17:55:53 +0300

    commit

---
+++ src/assets/blocks/main-page/main-page.js
@@ -0,0 +1,9 @@
+function getFileName () {
+
+    var file = document.getElementById ('uploaded-file').value;
+    
+    file = file.replace (/\\/g, «/»).split ('/').pop ();
+    
+    document.getElementById ('file-name').innerHTML = 'Имя файла: ' + file;
+    
+    }
