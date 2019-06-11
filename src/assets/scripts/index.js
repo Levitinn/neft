5b26c55e4b08f1999f051529877259636d970a2a
Nikita <nle@nkhstudio.ru>
2019-06-11 16:53:47 +0300

    commit

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function () {
     header('this is string to header.js')
+    
+    $(".invite__title").on("click", function() {  
+     
+        $(".main__invite").toggleClass("main__invite-active");    
+    });
+    
+    $(".invite__top-title").on("click", function() {  
+     
+        $(".main__invite").toggleClass("main__invite-active");    
+    });
+    
 })
