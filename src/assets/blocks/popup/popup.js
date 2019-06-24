02b9c5bad87b24f62b9b99454e0641155ba35156
Nikita <nle@nkhstudio.ru>
2019-06-24 17:51:49 +0300

    commit

--- src/assets/blocks/popup/popup.js
+++ src/assets/blocks/popup/popup.js
@@ -3,4 +3,7 @@ function popup() {
         var video = document.getElementsByClassName('popup__video');
         video[0].paused ? video[0].play() : video[0].pause();
     });
+    $('.popup__close').click(function(){
+        $('.popup').fadeOut();
+    })
 }
